f691aea200daf51f4842ff39287a3a4b5b52fc4e
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2019-07-17 00:27:39 +0200

    adding angular ui to activate datepicker for new event and finishing up repeat-x directive

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -30,7 +30,7 @@ eventsApp.controller('EditEventController', function ($scope,eventData) {
     };
 
     $scope.namePattern = /^(?:[A-Za-z]+)(?:[A-Za-z0-9 _]*)$/;
-    $scope.datePattern = /^([0]?[1-9]|[1|2][0-9]|[3][0|1])\/([0]?[1-9]|[1][0-2])\/([0-9]{4}|[0-9]{2})$/;
+    $scope.datePattern = /^([0]?[1-9]|[1][0-2])\/([0]?[1-9]|[1|2][0-9]|[3][0|1])\/([0-9]{4}|[0-9]{2})$/;
     $scope.numbersPattern = /^[0-9]*$/;
     $scope.lettersPattern = /^[a-zA-Z\s]*$/;
     $scope.addressPattern = /^(?:[A-Za-z0-9]+)(?:[A-Za-z0-9 ,.]*)$/;
--- app/js/directives/repeatx.js
+++ app/js/directives/repeatx.js
@@ -2,9 +2,22 @@
 
 eventsApp.directive('repeatX', function ($compile) {
     return {
-        link: function (scope, element, attributes, controller) {
+        // link: function (scope, element, attributes, controller) {
+        //     for (var i = 0; i < Number(attributes.repeatX) - 1; i++) {
+        //         element.after($compile(element.clone().attr('repeat-x', 0))(scope));
+        //     }
+        // }
+        compile: function (element, attributes) {
             for (var i = 0; i < Number(attributes.repeatX) - 1; i++) {
-                element.after($compile(element.clone().attr('repeat-x', 0))(scope));
+                element.after(element.clone().attr('repeat-x',0));
+            }
+
+            return function(scope, element, attrs, controller) {
+                attributes.$observe('text',function(newValue){
+                    if(newValue === 'Hello World'){
+                        element.css('color','red');
+                    }
+                });
             }
         }
     }
